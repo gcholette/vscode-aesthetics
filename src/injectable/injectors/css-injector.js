17ef7df5a9f7dea4d9c5f502326b24a70ac7e8dd
gcholette <solutions@gcholette.com>
2022-05-15 12:28:51 -0400

    fix: optimise injector accuracy, would not start in some cases

--- src/injectable/injectors/css-injector.js
+++ src/injectable/injectors/css-injector.js
@@ -1,45 +1,22 @@
-// append the constant customCssStr here
-// the extension combines the theme file containing this variable 
-// const customCssStr = ''
-const vscodeTokenSelector = '.vscode-tokens-styles'
-const universalNewlineRgx = /(\r\n|\n|\r)/gm
-
 
 const injectStyles = (stylesStr) => {
-  const currentStyles = document.querySelector(vscodeTokenSelector).innerText
-  const updatedStyles = `${currentStyles}${stylesStr}`
-
-  const updatedStyleTag = document.createElement('style')
-  updatedStyleTag.innerText = updatedStyles.replace(universalNewlineRgx, '')
+  const updatedStyleTag = document.createElement('style');
+  updatedStyleTag.innerText = stylesStr
   document.body.appendChild(updatedStyleTag)
 }
 
-const observeEditorLoading = (mutations, observer) => {
-  mutations.forEach(mutation => {
-    const { type } = mutation
-    const vscodeStyles = document.querySelector(vscodeTokenSelector)
-    const vscodeStylesText = document.querySelector(vscodeTokenSelector).innerText
-
-    if (vscodeStyles) {
-      switch (type) {
-        case 'attributes':
-          observer.disconnect()
-          observer.observe(vscodeStyles, { childList: true })
-          break;
-        case 'childList':
-          if (vscodeStylesText) {
-            injectStyles(customCssStr)
-            observer.disconnect()
-          }
-          break;
-      }
+const observeEditorLoading = (mutations) => {
+  mutations.forEach(x => {
+    const element = document.querySelector('.monaco-workbench')
+    if (element) {
+      injectStyles(customCssStr)
     }
   })
 }
 
 const init = () => {
-  const loadingObs = new MutationObserver(observeEditorLoading)
-  loadingObs.observe(document.querySelector('body'), { attributes: true })
+  const loadingObs = new MutationObserver(observeEditorLoading);
+  loadingObs.observe(document.querySelector('body'), { attributes: true });
 }
 
 (() => init())()
